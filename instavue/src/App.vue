052b3bfe6d7db60983acdfc0e6a31d3afb14e508
sonyejin <sonyejin6354@naver.com>
2024-02-16 14:57:13 +0900

    Vue 프로젝트 생성

---
+++ instavue/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <a>인스타그램 분석 서비스</a>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
